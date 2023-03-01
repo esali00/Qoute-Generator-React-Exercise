976864e111dc739c4358335b69fb87eb5c6a94c0
Emir Salihovic <e.salihovic@outlook.de>
2023-03-01 16:42:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [qoute, setQoute] = useState("")
+  const [author, setAuthor] = useState("")
+
+  const fetchData = async () => {
+    const random_num = Math.floor(Math.random() * 1643)
+    let uri = "https://type.fit/api/quotes"
+
+    await fetch(uri)
+      .then(
+        response => response.json() 
+      ).then(
+        json => {
+          let data = json[random_num] 
+          console.log(data)
+          setQoute(data.text)
+          setAuthor(data.author)
+        }
+      ).catch(
+        err => console.log(err, "couldnt fetch data")
+      )
+  }
+
+
+  useEffect( () => {
+    fetchData()
+    // console.log("i fire once")
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project 3: Qoute Generator</h1>
+      <div className="qoute-container">
+        <button onClick={() => {fetchData()}}>Next Qoute</button>
+        <p><b>"{ qoute }"</b></p>
+        <i>- { author }</i>
+      </div>
     </div>
   );
 }
